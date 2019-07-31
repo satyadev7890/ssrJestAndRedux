e392910203452f6ce4d357ff3552a02aaa26b0ea
Gaddam, Satyadev - Satyadev <vc.satyadev.gaddam@lowes.com>
2019-07-31 12:55:54 +0530

    light

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-    plugins: [ '@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread']
+    plugins: [ '@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread'],
+    ignore: ['node_modules', 'build']
 };
