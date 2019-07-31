b2780f1eb054a6568045128d6193dce993857fef
Gaddam, Satyadev - Satyadev <vc.satyadev.gaddam@lowes.com>
2019-07-31 12:08:21 +0530

    lighthouse changes

--- jest.config.js
+++ jest.config.js
@@ -12,8 +12,9 @@ module.exports = {
       "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
       "\\.(css|less)$": "identity-obj-proxy"
     },
-    transformIgnorePatterns: ["/lib/", "/node_modules/"],
+    transformIgnorePatterns: ["/lib/", "/node_modules/", "/node_modules/lighthouse"],
     testPathIgnorePatterns: ["/lib/", "/node_modules/"],
     verbose: true,
     setupFilesAfterEnv: ['<rootDir>/jestSetupTests.js'],
+
   };
